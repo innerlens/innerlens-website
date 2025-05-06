21d12fb8fdbd4fdbb88f68e14d4628816d68c63e
Gregory Conroy <gregory.conroy@bbd.co.za>
2025-05-06 23:12:14 +0200

    Added header

--- client/js/app.js
+++ client/js/app.js
@@ -1,3 +1,4 @@
+import { HeaderContent } from './components/header_content.js';
 import { TestPage } from './pages/test_page.js';
 import { Canvas } from './components/canvas.js';
 import { CanvasAnimator } from './logic/canvas_renderer.js';
@@ -5,24 +6,13 @@ import { CanvasAnimator } from './logic/canvas_renderer.js';
 const backgroundCanvas = new Canvas('background-canvas');
 backgroundCanvas.render(document.body);
 
-document.body.appendChild(document.createElement('main'));
+createStructure();
 
+// HeaderContent.render();
 TestPage.render();
 
 const canvasAnimator = new CanvasAnimator(backgroundCanvas.canvas); 
 canvasAnimator.start();
-
-// canvasAnimator.addParticle({x: -0.107,y: -0.194}, {x: 0,y: 0}, {x: 0,y: 0}, 374, {color: "#3F22BF"});
-// canvasAnimator.addParticle({x: -0.152,y: -0.047}, {x: 0,y: 0}, {x: 0,y: 0}, 181, {color: "#F2D06C"});
-// canvasAnimator.addParticle({x: -0.013,y: -0.117}, {x: 0,y: 0}, {x: 0,y: 0}, 317, {color: "#C57A48"});
-// canvasAnimator.addParticle({x: -0.192,y: -0.224}, {x: 0,y: 0}, {x: 0,y: 0}, 455, {color: "#F26C6C"});
-
-// canvasAnimator.addParticle({x: 0.718,y: 0.872}, {x: 0,y: 0}, {x: 0,y: 0}, 350, {color: "#F2D06C"});
-// canvasAnimator.addParticle({x: 0.801,y: 0.591}, {x: 0,y: 0}, {x: 0,y: 0}, 317, {color: "#C57A48"});
-// canvasAnimator.addParticle({x: 0.599,y: 0.757}, {x: 0,y: 0}, {x: 0,y: 0}, 352, {color: "#3F22BF"});
-// canvasAnimator.addParticle({x: 0.801,y: 0.499}, {x: 0,y: 0}, {x: 0,y: 0}, 352, {color: "#3F22BF"});
-// canvasAnimator.addParticle({x: 0.634,y: 0.560}, {x: 0,y: 0}, {x: 0,y: 0}, 376, {color: "#F26C6C"});
-
 
 canvasAnimator.addParticle({x: 0.830, y: 0.909}, {x: 0,y: 0}, {x: 0,y: 0}, 376, {color: "#F26C6C"});
 canvasAnimator.addParticle({x: 0.985, y: 0.825}, {x: 0,y: 0}, {x: 0,y: 0}, 352, {color: "#3F22BF"});
@@ -34,3 +24,9 @@ canvasAnimator.addParticle({x: 0.045, y: 0.023}, {x: 0,y: 0}, {x: 0,y: 0}, 455, 
 canvasAnimator.addParticle({x: 0.152, y: 0.085}, {x: 0,y: 0}, {x: 0,y: 0}, 317, {color: "#C57A48"});
 canvasAnimator.addParticle({x: 0.246, y: 0.085}, {x: 0,y: 0}, {x: 0,y: 0}, 181, {color: "#F2D06C"});
 canvasAnimator.addParticle({x: 0.087, y: 0}, {x: 0,y: 0}, {x: 0,y: 0}, 374, {color: "#3F22BF"});
+
+function createStructure() {
+    document.body.appendChild(document.createElement('header'));
+    document.body.appendChild(document.createElement('main'));
+    document.body.appendChild(document.createElement('footer'));
+}
---
+++ client/js/components/header_content.js
@@ -0,0 +1,25 @@
+export const HeaderContent = {
+    heading: "innerlens",
+    navLinks: [
+        {href: '#home', text: "Home"},
+        {href: '#my-personality', text: "My Personality"},
+        {href: '#personality-types', text: "Personality Types"}
+    ],
+
+    render() {
+        const navHtml = this.navLinks.map(link => `
+            <li class="nav-item">
+                <a class="nav-link" href="${link.href}">${link.text}</a>
+            </li>
+        `).join('');
+
+        const htmlContent = `
+            <h1 id="title">${this.heading}</h1>
+            <nav id="header-nav">
+                <ul id="header-ul">${navHtml}</ul>
+            </nav>
+        `;
+
+        document.querySelector('header').innerHTML = htmlContent;
+    }
+}
