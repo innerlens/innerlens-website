dde10ad4c3fd126940f47c5961261693d1ae00b8
Liam Barkley <Liam.Barkley@bbd.co.za>
2025-05-11 15:49:08 +0200

    updated port

--- server/server.js
+++ server/server.js
@@ -42,7 +42,7 @@ app.use((req, res, next) => {
   res.sendFile(path.join(__dirname, '../client/index.html'));
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = 8080;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
