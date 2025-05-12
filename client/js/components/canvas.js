6bc89caaf5fe489b4356f6d5e88f3ccca29bdb91
Vuyo Mama <65687571+KeloDraken@users.noreply.github.com>
2025-05-12 10:32:50 +0200

    Update canvas.js

--- client/js/components/canvas.js
+++ client/js/components/canvas.js
@@ -1,14 +1,14 @@
 export class Canvas {
-  constructor(canvasId) {
-    this.canvas;
-    this.parent;
-    this.canvasId = canvasId;
-  }
+    constructor(canvasId) {
+        this.canvas;
+        this.parent;
+        this.canvasId = canvasId;
+    }
 
-  render(parent) {
-    this.canvas = document.createElement("canvas");
-    this.canvas.id = this.canvasId;
-
-    parent.appendChild(this.canvas);
-  }
+    render(parent) {
+        this.canvas = document.createElement('canvas');
+        this.canvas.id = this.canvasId;
+        
+        parent.appendChild(this.canvas); 
+    }
 }
